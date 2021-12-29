96a52ea5fa026ab0ef1bd7e986a04c09197eec32
T-ooguri <bmff0326@outlook.jp>
2021-12-29 19:51:33 +0000

    laravel13

---
+++ create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.profile')
+
+@section('title','プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                 <h1>プロフィール作成</h1>
+                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <select class="form-control" name="gender">
+                                <option value="sel" {{ old('gender') === 'sel' ? 'selected' : ''}}>選択してください</option>
+                                <option value="men" {{ old('gender') === 'men' ? 'selected' : ''}}>男性</option>
+                                <option value="women" {{ old('gender') === 'women' ? 'selected' : ''}}>女性</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
